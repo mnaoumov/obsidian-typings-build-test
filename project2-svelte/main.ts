d7da673ea62ba0d961e4d24a22ea6361d2395b6b
Michael Naumov <mnaoumov@gmail.com>
2024-06-04 10:10:21 -0600

    Add project2-svelte

---
+++ project2-svelte/main.ts
@@ -0,0 +1,21 @@
+import {
+    App,
+} from "obsidian";
+
+function printInternalPlugins(app: App): void {
+    console.log(app.internalPlugins);
+}
+
+printInternalPlugins(app);
+
+import type {
+    InternalPlugins,
+} from "obsidian-typings";
+
+const internalPlugins: InternalPlugins = app.internalPlugins;
+
+console.log(internalPlugins);
+
+import { InternalPluginName } from "obsidian-typings/implementations";
+
+app.internalPlugins.getEnabledPluginById(InternalPluginName.FileExplorer);
