4d42623e374e405e0920785295904d9d75d0f033
Michael Naumov <mnaoumov@gmail.com>
2024-06-04 14:57:19 -0600

    no-types-setting

---
+++ no-types-setting/main.ts
@@ -0,0 +1,4 @@
+import { App } from "obsidian";
+
+const app2: App = window.app;
+console.log(app2.internalPlugins);
---
+++ no-types-setting/types.d.ts
@@ -0,0 +1,5 @@
+/**
+ * @note if you didn't set `types` setting in `tsconfig.json`, you have to explicitly import `obsidian-typings`
+ * in any project file included in the compilation
+ */
+import "obsidian-typings";
