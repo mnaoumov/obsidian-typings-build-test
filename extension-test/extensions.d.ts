ab913c04a430e11e5eee988d508f34ba1e28dd7e
Fevol <vernieuwefelix@gmail.com>
2024-07-29 20:00:14 +0200

    feat: plugin extensions example

---
+++ extension-test/extensions.d.ts
@@ -0,0 +1,14 @@
+import CommentatorPlugin from "./main";
+
+/**
+ * @note This is very important line, without it obsidian-typings definitions won't be picked up
+ */
+export {};
+
+declare module "obsidian-typings" {
+	interface Plugins {
+		plugins: {
+			"commentator": CommentatorPlugin;
+		};
+	}
+}
---
+++ extension-test/main.ts
@@ -0,0 +1,54 @@
+import { around } from "monkey-around";
+import {
+    App,
+    Plugin
+} from "obsidian";
+
+function printInternalPlugins(app: App): void {
+    console.log(app.internalPlugins);
+}
+
+printInternalPlugins(app);
+
+import {
+    InternalPlugins,
+    Plugins
+} from "obsidian-typings";
+
+const internalPlugins: InternalPlugins = app.internalPlugins;
+
+console.log(internalPlugins);
+
+import { InternalPluginName } from "obsidian-typings/implementations";
+
+app.internalPlugins.getEnabledPluginById(InternalPluginName.FileExplorer);
+
+interface Database {
+    dropDatabase(): Promise<void>;
+}
+
+class CommentatorPlugin extends Plugin {
+    remove_monkeys!: (() => void)[];
+    database!: Database;
+
+    someFunction() {
+        console.log("Test");
+    }
+
+    do() {
+        const z: Plugins = this.app.plugins;
+        z.plugins.commentator.someFunction();
+
+        this.remove_monkeys.push(around(this.app.plugins, {
+            // ERROR: Plugins does not have a method `uninstallPlugin`
+            uninstallPlugin: (oldMethod) => {
+                return async (id: string) => {
+                    oldMethod && await oldMethod.apply(this.app.plugins, [id]);
+                    if (id === "commentator")
+                        await this.database.dropDatabase();
+                };
+            },
+        }));
+    }
+}
+
